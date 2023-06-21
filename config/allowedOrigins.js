03b6b24472ccb4a71a95cd3bc0b2358927d947fd
altai861 <altaizandanbileg@gmail.com>
2023-06-21 09:33:26 +0800

    ready to deploy

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
     'http://localhost:5173',
+    'http://localhost:3000',
     'https://www.dandrepairshop.com',
     'https://dandrepairshop.com'
 ];
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const cors = require('cors');
 const corsOptions = require('./config/corsOptions');
 const connectDB = require('./config/dbConn');
 const mongoose = require('mongoose');
+require('express-async-errors')
 const PORT = process.env.PORT || 3500;
 
 
